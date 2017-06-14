d2d3cd57801544ea9b3b8418e5d4d190aae62b04
Tejas Viswanath <tjvrulz@gmail.com>
2017-06-15 02:53:32 +0530

    Added comments to C++ program

--- Serial.cpp
+++ Serial.cpp
@@ -1,4 +1,12 @@
+/*
+
+    C++ program to receive r,g,and b values as command line parameters and provide smooth transition
+    between 2 colors.
+
+*/
+
 #include<windows.h>
+#define INTERPOLATION_DELAY 500
 
 using namespace std;
 
@@ -35,6 +43,7 @@ int main(int argc,char** argv) {
 	DWORD dwritten=0;
 	OVERLAPPED osWrite = { 0 };
 
+    //create serial port
 	hComPort = CreateFile(lPortName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 	PortDCB.DCBlength = sizeof(DCB);
 	GetCommState(hComPort, &PortDCB);
@@ -44,23 +53,26 @@ int main(int argc,char** argv) {
 	PortDCB.StopBits = ONESTOPBIT;
 
 	SetCommState(hComPort, &PortDCB);
-	unsigned char outputArray[11];
+	unsigned char outputArray[11]; //final output array in bytes
 
-	if (argc != 7) return 1;
+	if (argc != 7) return 1; //check argument count
 
-	int r = to_int(argv[1]);
-	int g = to_int(argv[2]);
-	int b = to_int(argv[3]);
-	int or = to_int(argv[4]);
-	int og = to_int(argv[5]);
-	int ob = to_int(argv[6]);
+	int r = to_int(argv[1]); //red value
+	int g = to_int(argv[2]); //green value
+	int b = to_int(argv[3]); //blue value
+	int or = to_int(argv[4]); //old red value
+	int og = to_int(argv[5]); //old green value
+	int ob = to_int(argv[6]); //old blue value
 
-	for (int i = 0; i < 500;i++){
+	for (int i = 0; i < INTERPOLATION_DELAY;i++){ //loop runs once per ms (ideally)
 
+        //linear interpolation between colors
 		int nr = or + (r-or)*i/500.0;
 		int ng = og + (g-og)*i/500.0;
 		int nb = ob + (b-ob)*i/500.0;
 
+        // the Arduino receives 11 bytes serially where the second byte is green, the 6th is red and
+        // the 11th is blue. First byte is for synchronization.
 		outputArray[0] = 255;
 		outputArray[1] = (unsigned char)ng;
 		outputArray[2] = 255;
@@ -72,9 +84,9 @@ int main(int argc,char** argv) {
 		outputArray[8] = 255;
 		outputArray[9] = 255;
 		outputArray[10] = (unsigned char)nb;
-
+        //send data to serial port
 		WriteFile(hComPort, outputArray, 11, &dwritten, &osWrite);
-		Sleep(1);
+		Sleep(1); //sleep for 1ms
 	}
 
 
